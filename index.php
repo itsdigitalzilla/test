bf8760b1372e9239ce535747f08db794cbd831cf
itsdigitalzilla <108569227+itsdigitalzilla@users.noreply.github.com>
2022-07-03 11:36:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Digital Zilla | Home</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script src="bootstrap-3.3.7\dist\js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="style/mystyle.css">
+        <link rel="icon" href="images/logo1.jpeg" sizes="32x32" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+
+        <h1 style="text-align:center">Welcome to Digital Zilla</h1>
+
+
+	</body>
+</html>
